1c5d89001b3d7d79f8032965e1f8283986bd1006
Rupesh sahu <rupeshsahu969@gmail.com>
2023-12-23 08:09:14 +0530

    add hero pages

--- src/App.js
+++ src/App.js
@@ -7,24 +7,20 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
 
-
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
           <Route path="/" element={<Shop />} />
           <Route path="/mens" element={<ShopCategory category="men" />} />
           <Route path="/womens" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kids" />} />
-
           <Route path="/product" elements={<Product />}>
             <Route path=":productId" elements={<Product />} />
           </Route>
           <Route path="/cart" elements={<Cart />} />
-
           <Route path="/login" elements={<LoginSignUp />} />
         </Routes>
       </BrowserRouter>
